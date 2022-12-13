5922a61fbf1be21370aee585fd0227ca63798b1e
ict05-17 <ict05-17@DESKTOP-K36HJKG>
2022-12-13 18:24:11 +0900

    랜덤 수 만들기

--- test/src/test/ArrayTest.java
+++ test/src/test/ArrayTest.java
@@ -1,36 +1,40 @@
 package test;
-import java.util.Arrays;
+
+import java.util.Random;
 
 public class ArrayTest {
 
 	public static void main(String[] args) {
+
+		int[] nums = { 20, 5, 7, 98, 45, 7, 45, 2, 12, 47 };
+
+		// 값 출력
+		for (int i = 0; i < nums.length; i++) {
+			System.out.printf("%d", nums[i]);
+			if (i < nums.length - 1)
+				System.out.print(',');
+		}
 		
-		//ar1
-		int[] ar1 = new int[5];
-		ar1 = new int[7]; //ar1 이 새로운 배열을 참조하는것 가능
+		Random rand = new Random(); // Random은 처음보네
 		
 		
+		int s = rand.nextInt(10); // 0~10(미포함) 수 반환
+		int d = rand.nextInt(10); // 0~10(미포함) 수 반환
+		// nums에서 [0]공간의 값과 [2]공간의 값 바꾸기 -> 랜덤하게 섞기 위해 0,2라는 특정값 대신 변수화
 		
-		//5개짜리 정수를 담을 수 있는 배열을 이끄는 3개
-		int[][] ar2 = new int[3][5];
-		//C++이면  new int[3][5] 통짜이고,  3*5*4byte
-		//Java는 
+		int tmp;
+		tmp = nums[s];
+		nums[s] = nums[d];
+		nums[d] = tmp;
 		
-		ar2 = new int[2][6]; //가능
+		System.out.println();
 		
-		ar2[0] = new int[9]; //가능 길이가 달라져도 되네.
-		ar2[1] = new int[5];
-		/*
-		 톱니형 배열 (2차배열이 이렇게도 가능하기때문에 톱니형이라 불름.  c++은 2차원이 딱 정해졌는데, 자바는 이런게 가능)
-		 [0, 0, 0, 0, 0, 0, 0, 0, 0]
-		 [0, 0, 0, 0, 0]	
-		 
-		 
-		 */
-		
-		//이것도 가능
-		int[][] ar3 = new int[3][]; //위에처럼 데이터값을 나중에 넣을거라면, 이렇게 생성하는게 좋지
-		
+		// 값 출력
+		for (int i = 0; i < nums.length; i++) {
+			System.out.printf("%d", nums[i]);
+			if (i < nums.length - 1)
+				System.out.print(',');
+		}
 	}
 
 }
